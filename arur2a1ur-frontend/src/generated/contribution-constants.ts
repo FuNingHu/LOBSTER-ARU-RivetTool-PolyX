d9d37782d69fdb9828ba42ef62a1d81db86b5ada
FuNingHu <funing_hu@163.com>
2026-02-03 06:51:36 +0000

    update urcapx version to v1.1.2

--- arur2a1ur-frontend/src/generated/contribution-constants.ts
+++ arur2a1ur-frontend/src/generated/contribution-constants.ts
@@ -1,6 +1,6 @@
 // AUTO GENERATED - DO NOT EDIT
 export const VENDOR_ID = 'lobtex';
 export const URCAP_ID = 'ar-ur-2a1-ur';
-export const URCAP_VERSION = '1.1.0';
+export const URCAP_VERSION = '1.1.2';
 export const URCAP_ROS_NAMESPACE = 'Vendor_lobtex/URCap_ar_ur_2a1_ur';
 export const PATH = `/${VENDOR_ID}/${URCAP_ID}/arur2a1ur-frontend`;
